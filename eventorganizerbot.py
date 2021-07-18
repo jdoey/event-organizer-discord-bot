848bdbe47640021371c04375375c487878344f64
jdoey <70018021+jdoey@users.noreply.github.com>
2021-07-18 05:32:40 -0700

    Update eventorganizerbot.py
    added a feature that prevents the user from reacting to more than one emoji in the events channel

--- eventorganizerbot.py
+++ eventorganizerbot.py
@@ -6,10 +6,10 @@ from discord.ext import commands
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=';', intents=intents)
-bot_token = ''
+bot_token = 'ODY1OTQ0ODc1NTMxMTA4Mzc2.YPLYAg.9OmXCl2Lxpx7mh4NKdyAQ7HVTJA'
 
 # channel to send the embedded messages
-events_channel_id = ''
+events_channel_id = 865164812438732800
 
 messageID = None
 watched_messages = {
@@ -130,8 +130,12 @@ async def changeevent(ctx, eventName: str, eventDate: str, eventTime: str, ID: s
 async def on_reaction_add(reaction, user):
     # allows only specific reactions, unauthorized reactions will be deleted
     allowed_emojis = ["✅", "❌"]
-    if user.id != bot.user.id and reaction.emoji not in allowed_emojis:
-        await reaction.remove(user)
+    msg = reaction.message
+    # iterates through each reaction in message
+    for reacts in msg.reactions:
+        # checks if user is a bot and if the user reacted to two different emojis
+        if user in await reacts.users().flatten() and user.id != bot.user.id and str(reacts) != str(reaction.emoji):
+            await msg.remove_reaction(reacts.emoji, user)
     msgID = reaction.message.id
     # checks if the message has already been added to the dictionary
     if not msgID in watched_messages:
@@ -144,7 +148,7 @@ async def on_reaction_add(reaction, user):
     member = discord.utils.get(reaction.message.guild.members, id=user.id)
     # gets the role assigned to the message
     role = discord.utils.get(reaction.message.guild.roles, id=watched_messages[msgID]["✅"])
-    # removes the role from the user
+    # gives the user the corresponding role
     await member.add_roles(role)
 
 
