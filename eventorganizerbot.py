c60d4425cae033dd32df8b3707318041e50775ce
jdoey <70018021+jdoey@users.noreply.github.com>
2021-07-18 06:58:39 -0700

    Update eventorganizerbot.py

--- eventorganizerbot.py
+++ eventorganizerbot.py
@@ -7,10 +7,10 @@ from discord.ext import commands
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=';', intents=intents)
-bot_token = 'ODY1OTQ0ODc1NTMxMTA4Mzc2.YPLYAg.VZxcPOfujFqA6ah1vNDRKFwvcvI'
+bot_token = ''
 
 # channel to send the embedded messages
-events_channel_id = 865164812438732800
+events_channel_id = ''
 
 messageID = None
 watched_messages = {
